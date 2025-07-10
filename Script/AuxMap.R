a5cb1084bfec75d2bc44d0ca422126993a4c80e1
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-10 09:51:08 -0400

    Haciendo el mapa auxiliar para visualizar las ciudades de Oaxaca en un mapa

---
+++ Script/AuxMap.R
@@ -0,0 +1,32 @@
+
+municipios <- data.frame(
+  MUNICIPIO_RES = c("026", "067", "125", 
+                    "189", "247", "269", 
+                    "397", "417", "455",
+                    "521", "030", "093",
+                    "250", "570", "012",
+                    "334", "364", "028"
+                    ), 
+  nombre_municipio = c("CHALCATONGO DE HIDALGO","OAXACA DE JUÁREZ","SAN CARLOS YAUTEPEC",
+                       "SAN JUAN COMALTEPEC", "CAPULÁLPAM DE MÉNDEZ", "SAN MIGUEL EL GRANDE",
+                       "HEROICA CIUDAD DE TLAXIACO", "SANTA MARÍA JACATEPEC", "SANTIAGO AYUQUILILLA",
+                       "SANTO DOMINGO TONALTEPEC", "EL ESPINAL", "SAN ANDRÉS LAGUNAS",
+                       "SAN MATEO ETLATONGO", "ZIMATLÁN DE ÁLVAREZ", "CANDELARIA LOXICHA",
+                       "VILLA DE TUTUTEPEC", "SANTA CATARINA JUQUILA", "HEROICA CIUDAD DE EJUTLA DE CRESPO"
+                       ),
+  lat = c(17.033, 17.0654, 16.49699,
+          17.3451, 17.31944, 17.0467,
+          17.2691, 17.8602, 17.9341,
+          17.6105, 16.48472, 17.5736,
+          17.41609, 16.86939, 15.92801,
+          16.11747, 16.23819, 16.56305
+          ),
+  long = c(-97.583, -96.7237, -96.10648,
+           -95.9527, -96.42777, -97.6222,
+           -97.68053, -96.2099, -97.9580,
+           -97.3583, -95.04167, -97.52277,
+           -97.27389, -96.78435, -96.51749,
+           -97.59748, -97.29161, -96.7325
+           )
+)
+
